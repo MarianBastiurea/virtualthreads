97b02f44f1fe1420923a2be5b0e5c91a47abd200
MarianBastiurea <marian.bastiurea@gmail.com>
2025-08-23 07:06:29 +0100

    structured scope

---
+++ virtualthreads/src/main/java/com/marianbastiurea/section09/Lec07ScopedValuesWithStructuredTaskScope.java
@@ -0,0 +1,62 @@
+package com.marianbastiurea.section09;
+
+import com.marianbastiurea.util.CommonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Lec07ScopedValuesWithStructuredTaskScope {
+    private static final Logger logger = LoggerFactory.getLogger(Lec07ScopedValuesWithStructuredTaskScope.class);
+    private static final ScopedValue<String> SESSION_TOKEN = ScopedValue.newInstance();
+
+    public static void main(String[] args) {
+
+        ScopedValue.runWhere(SESSION_TOKEN, "token-123", Lec07ScopedValuesWithStructuredTaskScope::task);
+
+    }
+
+    private static void task(){
+        try(var taskScope = new StructuredTaskScope<>()){
+
+            logger.info("token: {}", SESSION_TOKEN.get());
+
+            var subtask1 = taskScope.fork(Lec07ScopedValuesWithStructuredTaskScope::getDeltaAirfare);
+            var subtask2 = taskScope.fork(Lec07ScopedValuesWithStructuredTaskScope::getFrontierAirfare);
+
+            taskScope.join();
+
+            logger.info("subtask1 state: {}", subtask1.state());
+            logger.info("subtask2 state: {}", subtask2.state());
+
+            logger.info("subtask1 result: {}", subtask1.get());
+            logger.info("subtask2 result: {}", subtask2.get());
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getDeltaAirfare(){
+        var random = ThreadLocalRandom.current().nextInt(100, 1000);
+        logger.info("delta: {}", random);
+        logger.info("token: {}", SESSION_TOKEN.get());
+        CommonUtil.sleep("delta", Duration.ofSeconds(1));
+        return "Delta-$" + random;
+    }
+
+    private static String getFrontierAirfare(){
+        var random = ThreadLocalRandom.current().nextInt(100, 1000);
+        logger.info("frontier: {}", random);
+        logger.info("token: {}", SESSION_TOKEN.get());
+        CommonUtil.sleep("frontier", Duration.ofSeconds(2));
+        return "Frontier-$" + random;
+    }
+
+    private static String failingTask(){
+        throw new RuntimeException("oops");
+    }
+
+}
